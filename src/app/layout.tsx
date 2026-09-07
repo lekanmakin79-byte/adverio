843925e93067e1b5f858e896322f1e843edb1545
Olaand WebStudio <lekanmakin79@gmail.com>
2026-09-07 21:21:15 +0100

    Add Google Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://adverio.vercel.app"),
+  
+    verification: {
+    google: "Uk6-ipqkCWL46dR82EDaUmp2KomfSn4Ulm4wEQhxpa0",
+  },
 
   title: {
     default: "Adverio | AI Marketing Automation for Small Businesses",
